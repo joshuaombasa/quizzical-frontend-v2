26ef0f46b1d01d13b21bb552736ced954e7f5158
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-01 20:45:32 +0300

    added answers.jsx

---
+++ src/pages/Asnwers.jsx
@@ -0,0 +1,9 @@
+import React from "react";
+
+export default function Answers() {
+    return (
+        <div className="answers--container">
+            <h1>This is answers page</h1>
+        </div>
+    )
+}
