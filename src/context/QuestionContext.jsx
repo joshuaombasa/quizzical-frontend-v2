913be5991774180eea32feaeb48331e3ba4cd08a
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-01 20:45:58 +0300

    added context provider

---
+++ src/context/QuestionContext.jsx
@@ -0,0 +1,84 @@
+import React, { createContext } from "react";
+import { v4 as uuidv4 } from 'uuid';
+const URL = "https://opentdb.com/api.php?amount=10&category=18&difficulty=hard&type=multiple"
+
+const questionsContext = createContext()
+
+function QuestionsContextProvider(props) {
+
+
+    const [questionsData, setQuestionsData] = React.useState([])
+
+    function shuffleArray(array) {
+        const shuffledArray = [...array]; 
+    
+        for (let i = shuffledArray.length - 1; i > 0; i--) {
+            const j = Math.floor(Math.random() * (i + 1));
+            [shuffledArray[i], shuffledArray[j]] = [shuffledArray[j], shuffledArray[i]];
+        }
+    
+        return shuffledArray; 
+    }
+    
+    function updateAnswerState(id) {
+        setQuestionsData(prevQuestionsData => {
+           return prevQuestionsData.map(item => {
+                const newAnswersArray = item.answers.map(answer => {
+                    return answer.answerId === id ? {...answer, isSelected : !answer.isSelected} : answer
+                })
+
+                return {
+                    ...item,
+                    answers : newAnswersArray
+                }
+            })
+
+        })
+    }
+
+
+    React.useEffect(() => {
+        const getQuestions = async () => {
+            try {
+                const res = await fetch(URL)
+                const data = await res.json()
+                const rawData = data.results
+                const enrichedDatawithId = rawData.map(item => (
+                    {
+                        correct_answer: item.correct_answer,
+                        question: item.question,
+                        answers: shuffleArray([...item.incorrect_answers, item.correct_answer]),
+                        id: uuidv4()
+                    }
+                ))
+                const answersWithState = enrichedDatawithId.map(item => {
+                    const answersUpdated = item.answers.map(answer => {
+                        return {answer : answer, isSelected : false, answerId: uuidv4()}
+                    })
+                    return {
+                        ...item,
+                        answers : answersUpdated
+                    }
+                })
+                console.log(answersWithState)
+                setQuestionsData(answersWithState)
+            } catch (error) {
+                console.log(error)
+            }
+        }
+
+        getQuestions()
+    }, [])
+
+    // console.log(questionsData)
+
+    return (
+        <questionsContext.Provider 
+           value={{ questionsData: questionsData, updateAnswerState: updateAnswerState }}
+        >
+            {props.children}
+        </questionsContext.Provider>
+    )
+}
+
+export { QuestionsContextProvider, questionsContext }
