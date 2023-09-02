8ed758611382407f0d4429bc02f105f940df48b2
Joshua Ombasa <nyambegaombasa@gmail.com>
2023-09-02 21:28:47 +0300

    updated parent container

--- src/pages/Welcome.jsx
+++ src/pages/Welcome.jsx
@@ -3,11 +3,13 @@ import { Link } from "react-router-dom";
 
 export default function Welcome() {
     return (
-        <div className="welcome--container">
-            <div className="wrapper">
-                <h1>Quizzical</h1>
-                <p>"Welcome to Quizzical ! 🌟 Get ready to test your knowledge and have fun with our quizzes. Start your learning journey now!"</p>
-                <Link to="/questions">Start quiz</Link>
+        <div className="container">
+            <div className="welcome--container">
+                <div>
+                    <h1>Quizzical</h1>
+                    <p>"Welcome to Quizzical! 🌟 Get ready to test your knowledge and have fun with our quizzes. Start your learning journey now!"</p>
+                    <Link to="/questions">Start quiz</Link>
+                </div>
             </div>
         </div>
     )
